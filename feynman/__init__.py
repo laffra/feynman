2a357bfda8235fed1280221f31737edb1d156fae
laffra <laffra@gmail.com>
2022-03-26 18:02:23 +0100

    Handle return value

--- feynman/__init__.py
+++ feynman/__init__.py
@@ -3,7 +3,6 @@ Explain complex things in simple terms
 """
 
 from concurrent.futures import thread
-import functools
 import inspect
 import os
 import re
@@ -21,6 +20,7 @@ module = re.compile(r"<module>")
 last_flush = time.time()
 updates = { }
 config = {}
+return_value = None
 
 UPDATE_FLUSH_INTERVAL = 0.05
 
@@ -49,14 +49,16 @@ def image(left, top, width, height, src, id=""):
 def line(x1, y1, x2, y2, color="black", stroke=2, id=""):
     feynman.server.send(create_event("line", locals()))
 
+def marker(text):
+    feynman.server.send(create_event("marker", locals()))
+
 def flush_updates(exit=False):
-    global last_flush
     when = time.time()
     if exit or when - last_flush > UPDATE_FLUSH_INTERVAL:
         for key in updates:
             feynman.server.send(create_event("update", updates[key]))
         updates.clear()
-        last_flush = when
+        feynman.last_flush = when
     if exit:
         threading.Thread(target=lambda: (time.sleep(2), os._exit(0))).start()
 
@@ -95,28 +97,30 @@ class Explain(object):
         self.code_cache[code] = args
         return args
 
-    def explain(self, name, frame):
+    def explain(self, name, frame, return_value, args):
         if name in config:
-            config[name](**frame.f_locals)
+            feynman.return_value = return_value
+            config[name](*[frame.f_locals[arg] for arg in args])
         
-    def handle_call(self, frame):
+    def handle_return(self, frame, return_value):
         module_name = frame.f_globals["__name__"]
         if module_name == "feynman": return
         function_name = frame.f_code.co_name
         self.send_info("module", module_name)
+        args = self.getargs(frame.f_code)
         try:
             class_name = "%s.%s" % (module_name, frame.f_locals['self'].__class__.__name__)
             self.send_info("class", class_name)
             name = "%s.%s" % (class_name, function_name)
-            self.send_info("method", "%s %s" % (name, " ".join(self.getargs(frame.f_code))))
+            self.send_info("method", "%s %s" % (name, " ".join(args)))
         except Exception as e:
             name = "%s.%s" % (module_name, function_name)
-            self.send_info("function", "%s %s" % (name, " ".join(self.getargs(frame.f_code))))
-        self.explain(name, frame)
+            self.send_info("function", "%s %s" % (name, " ".join(args)))
+        self.explain(name, frame, return_value, args)
 
-    def handle_event(self, frame, event, _):
+    def handle_event(self, frame, event, arg):
         if event == "return":
-            self.handle_call(frame)
+            self.handle_return(frame, arg)
         return self.handle_event
 
     def __enter__(self):
